7b9fcb3f0e8809648fae52152ef6417d0f2c274d
Youchen Zhao <33065689+zyouc518@users.noreply.github.com>
2018-08-12 02:51:59 +0800

    Add files via upload

--- js/alarmjs.js
+++ js/alarmjs.js
@@ -43,7 +43,7 @@
 
         //二级报警
         var alarm2water = 0;
-        var alarm2pressure = 0;
+        var alarm2waterure = 0;
         var alarm2strain = 0;
         var waterflag = 0;
         if (aar[0] > 20) {
@@ -68,28 +68,28 @@
             alarm2water = 1;
         }
 
-        var pressurealarm = 0;
+        var waterurealarm = 0;
         if (((aar[0] - aar[1]) / aar[1]) > 0.25) {
-            pressurealarm++;
+            waterurealarm++;
         }
         if (((aar2[0] - aar2[1]) / aar2[1]) > 0.25) {
-            pressurealarm++;
+            waterurealarm++;
         }
         if (((aar3[0] - aar3[1]) / aar3[1]) > 0.25) {
-            pressurealarm++;
+            waterurealarm++;
         }
         if (((aar4[0] - aar4[1]) / aar4[1]) > 0.25) {
-            pressurealarm++;
+            waterurealarm++;
         }
         if (((aar5[0] - aar5[1]) / aar5[1]) > 0.25) {
-            pressurealarm++;
+            waterurealarm++;
         }
         if (((aar6[0] - aar6[1]) / aar6[1]) > 0.25) {
-            pressurealarm++;
+            waterurealarm++;
         }
 
-        if (pressurealarm > 3) {
-            alarm2pressure = 1;
+        if (waterurealarm > 3) {
+            alarm2waterure = 1;
         }
 
         var strainalarm = 0;
@@ -116,7 +116,7 @@
             alarm2strain = 1;
         }
 
-        if (alarm2water && (alarm2pressure || alarm2strain)) {
+        if (alarm2water && (alarm2waterure || alarm2strain)) {
             //二级报警操作
             document.getElementById('header2').className = "main";
         }
@@ -140,6 +140,368 @@
             //三级报警操作
             document.getElementById('header3').className = "main";
         }
+
+
+
+    var water1=0; 
+    var water1a=0;
+    var waterflag=0;
+    for (var k=0;k<300;k++)
+    {
+        if (ar[k]!=null)
+        {
+            water1=1;
+        }
+        if (ar[k]>100)
+        {
+            water1a=1;
+        }
+    }
+    if (water1==0 || water1a==1)
+    {
+        waterflag++;
+    }
+
+
+    var water2=0; 
+    var water2a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (ar2[k]!=null)
+        {
+            water2=1;
+        }
+        if (ar2[k]>100)
+        {
+            water2a=1;
+        }
+        //if (ar2[k]>=64)调试语句，是可行的
+        //{water2++;}
+    }
+    if (water1==0 || water2a==1)
+    {
+        waterflag++;
+    }
+
+    var water3=0; 
+    var water3a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (ar3[k]!=null)
+        {
+            water3=1;
+        }
+        if (ar3[k]>100)
+        {
+            water3a=1;
+        }
+    }
+    if (water3==0 || water3a==1)
+    {
+        waterflag++;
+    }
+
+
+    var water4=0; 
+    var water4a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (ar4[k]!=null)
+        {
+            water4=1;
+        }
+        if (ar4[k]>100)
+        {
+            water4a=1;
+        }
+    }
+    if (water4==0 || water4a==1)
+    {
+        waterflag++;
+    }
+
+    var water5=0; 
+    var water5a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (ar5[k]!=null)
+        {
+            water5=1;
+        }
+        if (ar5[k]>100)
+        {
+            water5a=1;
+        }
+    }
+    if (water5==0 || water5a==1)
+    {
+        waterflag++;
+    }
+
+    var water6=0; 
+    var water6a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (ar6[k]!=null)
+        {
+            water6=1;
+        }
+        if (ar6[k]>100)
+        {
+            water6a=1;
+        }
+    }
+    if (water6==0 || water6a==1)
+    {
+        waterflag++;
+    }
+
+
+    if (waterflag>0)
+    {
+        document.getElementById('header7').className = "main";  
+    }
+
+
+    var press1=0; 
+    var press1a=0;
+    var pressflag=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aar[k]!=null)
+        {
+            press1=1;
+        }
+        if (aar[k]>2000)
+        {
+            press1a=1;
+        }
+    }
+    if (press1==0 || press1a==1)
+    {
+pressflag++;
+    }
+
+
+    var press2=0; 
+    var press2a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aar2[k]!=null)
+        {
+            press2=1;
+        }
+        if (aar2[k]>2000)
+        {
+            press2a=1;
+        }
+        //if (aar2[k]>=64)调试语句，是可行的
+        //{press2++;}
+    }
+    if (press1==0 || press2a==1)
+    {
+pressflag++;
+    }
+
+    var press3=0; 
+    var press3a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aar3[k]!=null)
+        {
+            press3=1;
+        }
+        if (aar3[k]>2000)
+        {
+            press3a=1;
+        }
+    }
+    if (press3==0 || press3a==1)
+    {
+pressflag++;
+    }
+
+
+    var press4=0; 
+    var press4a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aar4[k]!=null)
+        {
+            press4=1;
+        }
+        if (aar4[k]>2000)
+        {
+            press4a=1;
+        }
+    }
+    if (press4==0 || press4a==1)
+    {
+pressflag++;
+    }
+
+    var press5=0; 
+    var press5a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aar5[k]!=null)
+        {
+            press5=1;
+        }
+        if (aar5[k]>2000)
+        {
+            press5a=1;
+        }
+    }
+    if (press5==0 || press5a==1)
+    {
+pressflag++;
+    }
+
+    var press6=0; 
+    var press6a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aar6[k]!=null)
+        {
+            press6=1;
+        }
+        if (aar6[k]>2000) //(aar6[k]>2)
+        {
+            press6a=1;
+        }
+    }
+    if (press6==0 || press6a==1)
+    {
+pressflag++;
+    }
+
+    if (pressflag>0)
+    {
+        document.getElementById('header8').className = "main";  
+    }
+
+
+    var strain1=0; 
+    var strain1a=0;
+    var strainflag=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aaar[k]!=null)
+        {
+            strain1=1;
+        }
+        if (aaar[k]>2000)//(aaar[k]>2)
+        {
+            strain1a=1;
+        }
+        //if (aaar[k]>2)
+        //{strain1a=strain1a+1;}
+    }
+    if (strain1==0 || strain1a==1) //if (strain1a==300)
+    {
+strainflag++;
+    }
+
+
+    var strain2=0; 
+    var strain2a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aaar2[k]!=null)
+        {
+            strain2=1;
+        }
+        if (aaar2[k]>2000)
+        {
+            strain2a=1;
+        }
+        //if (aaar2[k]>=64)调试语句，是可行的
+        //{strain2++;}
+    }
+    if (strain1==0 || strain2a==1)
+    {
+strainflag++;
+    }
+
+    var strain3=0; 
+    var strain3a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aaar3[k]!=null)
+        {
+            strain3=1;
+        }
+        if (aaar3[k]>2000)
+        {
+            strain3a=1;
+        }
+    }
+    if (strain3==0 || strain3a==1)
+    {
+strainflag++;
+    }
+
+
+    var strain4=0; 
+    var strain4a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aaar4[k]!=null)
+        {
+            strain4=1;
+        }
+        if (aaar4[k]>2000)
+        {
+            strain4a=1;
+        }
+    }
+    if (strain4==0 || strain4a==1)
+    {
+strainflag++;
+    }
+
+    var strain5=0; 
+    var strain5a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aaar5[k]!=null)
+        {
+            strain5=1;
+        }
+        if (aaar5[k]>2000)
+        {
+            strain5a=1;
+        }
+    }
+    if (strain5==0 || strain5a==1)
+    {
+strainflag++;
+    }
+
+    var strain6=0; 
+    var strain6a=0;
+    for (var k=0;k<300;k++)
+    {
+        if (aaar6[k]!=null)
+        {
+            strain6=1;
+        }
+        if (aaar6[k]>2000) //(aar6[k]>2)
+        {
+            strain6a=1;
+        }
+    }
+    if (strain6==0 || strain6a==1)
+    {
+strainflag++;
+    }
+
+
+    if (strainflag>0)
+    {
+        document.getElementById('header9').className = "main";  
+    }
 
 
 
