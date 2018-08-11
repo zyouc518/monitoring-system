b13cfdcb318d81d9f72527b20f12b8946bc458fb
Wang Shuhan <harrywsh@126.com>
2018-08-12 01:43:07 +0800

    alert system

--- alarmjs.js
+++ alarmjs.js
@@ -1,11 +1,11 @@
 ﻿$(document).ready(function () {
     $.ajaxSetup({
         cache: false
-    }); 
+    });
 
 
 
-        setInterval(function () {
+    setInterval(function () {
 
         $('#results').load('php/g1-1.php');
         $('#results').load('php/g1-2.php');
@@ -26,93 +26,121 @@
         $('#results').load('php/g3-5.php');
         $('#results').load('php/g3-6.php');
 
-        var alarm1flag=0;
-        var TML=3;
-                 //var TML= a*b/(a*b+1-a); //这里需要完善，拿到页面的输入
-            if (ar[0]>TML) alarm1flag++;
-            if (ar2[0]>TML) alarm1flag++;
-            if (ar3[0]>TML) alarm1flag++;
-            if (ar4[0]>TML) alarm1flag++;
-            if (ar5[0]>TML) alarm1flag++;
-            if (ar6[0]>TML) alarm1flag++;
+        var alarm1flag = 0;
+        var a = document.getElementById('rho').value;
+        var b = document.getElementById('waterper').value;
+        var TML = a * b / (a * b + 1 - a); //这里需要完善，拿到页面的输入
+        if (ar[0] > TML) alarm1flag++;
+        if (ar2[0] > TML) alarm1flag++;
+        if (ar3[0] > TML) alarm1flag++;
+        if (ar4[0] > TML) alarm1flag++;
+        if (ar5[0] > TML) alarm1flag++;
+        if (ar6[0] > TML) alarm1flag++;
 
-            //if (alarm1flag>=0)
-            //{
-                //一级报警操作
-                document.getElementById('header1').style.color = "red";
-            //}
+        if (alarm1flag > 0) {
+            //一级报警操作
+            document.getElementById('header1').className = "main";
+        }
 
-            //二级报警
-            var alarm2water=0;
-            var alarm2pressure=0;
-            var alarm2strain=0;
-            var waterflag=0;
-            if (aar[0]>20){waterflag++;}
-            if (aar2[0]>20){waterflag++;}
-            if (aar3[0]>20){waterflag++;}
-            if (aar4[0]>20){waterflag++;}
-            if (aar5[0]>20){waterflag++;}
-            if (aar6[0]>20){waterflag++;}
-            if ((waterflag/6)>0.75)
-            {
-                alarm2water=1;
+        //二级报警
+        var alarm2water = 0;
+        var alarm2pressure = 0;
+        var alarm2strain = 0;
+        var waterflag = 0;
+        if (aar[0] > 20) {
+            waterflag++;
+        }
+        if (aar2[0] > 20) {
+            waterflag++;
+        }
+        if (aar3[0] > 20) {
+            waterflag++;
+        }
+        if (aar4[0] > 20) {
+            waterflag++;
+        }
+        if (aar5[0] > 20) {
+            waterflag++;
+        }
+        if (aar6[0] > 20) {
+            waterflag++;
+        }
+        if ((waterflag / 6) > 0.75) {
+            alarm2water = 1;
+        }
+
+        var pressurealarm = 0;
+        if (((aar[0] - aar[1]) / aar[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar2[0] - aar2[1]) / aar2[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar3[0] - aar3[1]) / aar3[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar4[0] - aar4[1]) / aar4[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar5[0] - aar5[1]) / aar5[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar6[0] - aar6[1]) / aar6[1]) > 0.25) {
+            pressurealarm++;
+        }
+
+        if (pressurealarm > 3) {
+            alarm2pressure = 1;
+        }
+
+        var strainalarm = 0;
+        if (((aaar[0] - aaar[1]) / aaar[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar2[0] - aaar2[1]) / aaar2[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar3[0] - aaar3[1]) / aaar3[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar4[0] - aaar4[1]) / aaar4[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar5[0] - aaar5[1]) / aaar5[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar6[0] - aaar6[1]) / aaar6[1]) > 0.2) {
+            strainalarm++;
+        }
+
+        if (strainalarm > 3) {
+            alarm2strain = 1;
+        }
+
+        if (alarm2water && (alarm2pressure || alarm2strain)) {
+            //二级报警操作
+            document.getElementById('header2').className = "main";
+        }
+
+        //三级报警
+        var alarm3 = 0;
+        var alarm3flag = 0;
+        for (var k = 0; k < 100; k++) {
+            if (ar[k] > 99) {
+                alarm3flag++;
             }
-
-            var pressurealarm=0;
-            if (((aar[0]-aar[1])/aar[1])>0.25){pressurealarm++;}
-            if (((aar2[0]-aar2[1])/aar2[1])>0.25){pressurealarm++;}
-            if (((aar3[0]-aar3[1])/aar3[1])>0.25){pressurealarm++;}
-            if (((aar4[0]-aar4[1])/aar4[1])>0.25){pressurealarm++;}
-            if (((aar5[0]-aar5[1])/aar5[1])>0.25){pressurealarm++;}
-            if (((aar6[0]-aar6[1])/aar6[1])>0.25){pressurealarm++;}
-
-            if (pressurealarm>3)
+            if (ar4[k] > 99) //最上层是传感器1和4
             {
-                alarm2pressure=1;
+                alarm3flag++;
             }
-
-            var strainalarm=0;
-            if (((aaar[0]-aaar[1])/aaar[1])>0.2){strainalarm++;}
-            if (((aaar2[0]-aaar2[1])/aaar2[1])>0.2){strainalarm++;}
-            if (((aaar3[0]-aaar3[1])/aaar3[1])>0.2){strainalarm++;}
-            if (((aaar4[0]-aaar4[1])/aaar4[1])>0.2){strainalarm++;}
-            if (((aaar5[0]-aaar5[1])/aaar5[1])>0.2){strainalarm++;}
-            if (((aaar6[0]-aaar6[1])/aaar6[1])>0.2){strainalarm++;}
-
-            if (strainalarm>3)
-            {
-                alarm2strain=1;
-            }
-
-            if (alarm2water && (alarm2pressure|| alarm2strain))
-            {
-                //二级报警操作
-                document.getElementById('header2').style.color = "red";
-            }
-
-            //三级报警
-            var alarm3=0;
-            var alarm3flag=0;
-            for (var k=0;k<100;k++)
-            {
-                if (ar[k]>99)
-                {
-                    alarm3flag++;
-                }
-                if (ar4[k]>99) //最上层是传感器1和4
-                {
-                    alarm3flag++;
-                }
-            }
-            if (alarm3flag>100)
-            {
-                alarm3=1;
-            }
-            if (alarm3==1)
-            {
-                //三级报警操作
-                document.getElementById('header3').className = "main";
-            }
+        }
+        if (alarm3flag > 100) {
+            alarm3 = 1;
+        }
+        if (alarm3 == 1) {
+            //三级报警操作
+            document.getElementById('header3').className = "main";
+        }
 
 
 
--- js/alarmjs.js
+++ js/alarmjs.js
@@ -1,11 +1,11 @@
 ﻿$(document).ready(function () {
     $.ajaxSetup({
         cache: false
-    }); 
+    });
 
 
 
-        setInterval(function () {
+    setInterval(function () {
 
         $('#results').load('php/g1-1.php');
         $('#results').load('php/g1-2.php');
@@ -26,176 +26,120 @@
         $('#results').load('php/g3-5.php');
         $('#results').load('php/g3-6.php');
 
-        var alarm1flag=0;
-        var TML=3;
-                 //var TML= a*b/(a*b+1-a); //这里需要完善，拿到页面的输入
-            if (ar[0]>TML) alarm1flag++;
-            if (ar2[0]>TML) alarm1flag++;
-            if (ar3[0]>TML) alarm1flag++;
-            if (ar4[0]>TML) alarm1flag++;
-            if (ar5[0]>TML) alarm1flag++;
-            if (ar6[0]>TML) alarm1flag++;
+        var alarm1flag = 0;
+        var a = document.getElementById('rho').value;
+        var b = document.getElementById('waterper').value;
+        var TML = a * b / (a * b + 1 - a); //这里需要完善，拿到页面的输入
+        if (ar[0] > TML) alarm1flag++;
+        if (ar2[0] > TML) alarm1flag++;
+        if (ar3[0] > TML) alarm1flag++;
+        if (ar4[0] > TML) alarm1flag++;
+        if (ar5[0] > TML) alarm1flag++;
+        if (ar6[0] > TML) alarm1flag++;
+        if (alarm1flag >= 4) {
+            //一级报警操作
+            document.getElementById('header1').className = "main";
+        }
 
-            if (alarm1flag>=4)
-            {
-                //一级报警操作
-                document.getElementById('header1').className = "main";
+        //二级报警
+        var alarm2water = 0;
+        var alarm2pressure = 0;
+        var alarm2strain = 0;
+        var waterflag = 0;
+        if (aar[0] > 20) {
+            waterflag++;
+        }
+        if (aar2[0] > 20) {
+            waterflag++;
+        }
+        if (aar3[0] > 20) {
+            waterflag++;
+        }
+        if (aar4[0] > 20) {
+            waterflag++;
+        }
+        if (aar5[0] > 20) {
+            waterflag++;
+        }
+        if (aar6[0] > 20) {
+            waterflag++;
+        }
+        if ((waterflag / 6) > 0.75) {
+            alarm2water = 1;
+        }
+
+        var pressurealarm = 0;
+        if (((aar[0] - aar[1]) / aar[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar2[0] - aar2[1]) / aar2[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar3[0] - aar3[1]) / aar3[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar4[0] - aar4[1]) / aar4[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar5[0] - aar5[1]) / aar5[1]) > 0.25) {
+            pressurealarm++;
+        }
+        if (((aar6[0] - aar6[1]) / aar6[1]) > 0.25) {
+            pressurealarm++;
+        }
+
+        if (pressurealarm > 3) {
+            alarm2pressure = 1;
+        }
+
+        var strainalarm = 0;
+        if (((aaar[0] - aaar[1]) / aaar[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar2[0] - aaar2[1]) / aaar2[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar3[0] - aaar3[1]) / aaar3[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar4[0] - aaar4[1]) / aaar4[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar5[0] - aaar5[1]) / aaar5[1]) > 0.2) {
+            strainalarm++;
+        }
+        if (((aaar6[0] - aaar6[1]) / aaar6[1]) > 0.2) {
+            strainalarm++;
+        }
+
+        if (strainalarm > 3) {
+            alarm2strain = 1;
+        }
+
+        if (alarm2water && (alarm2pressure || alarm2strain)) {
+            //二级报警操作
+            document.getElementById('header2').className = "main";
+        }
+
+        //三级报警
+        var alarm3 = 0;
+        var alarm3flag = 0;
+        for (var k = 0; k < 100; k++) {
+            if (ar[k] > 99) {
+                alarm3flag++;
             }
-
-            //二级报警
-            var alarm2water=0;
-            var alarm2pressure=0;
-            var alarm2strain=0;
-            var waterflag=0;
-            if (aar[0]>20){waterflag++;}
-            if (aar2[0]>20){waterflag++;}
-            if (aar3[0]>20){waterflag++;}
-            if (aar4[0]>20){waterflag++;}
-            if (aar5[0]>20){waterflag++;}
-            if (aar6[0]>20){waterflag++;}
-            if ((waterflag/6)>0.75)
+            if (ar4[k] > 99) //最上层是传感器1和4
             {
-                alarm2water=1;
+                alarm3flag++;
             }
-
-            var pressurealarm=0;
-            if (((aar[0]-aar[1])/aar[1])>0.25){pressurealarm++;}
-            if (((aar2[0]-aar2[1])/aar2[1])>0.25){pressurealarm++;}
-            if (((aar3[0]-aar3[1])/aar3[1])>0.25){pressurealarm++;}
-            if (((aar4[0]-aar4[1])/aar4[1])>0.25){pressurealarm++;}
-            if (((aar5[0]-aar5[1])/aar5[1])>0.25){pressurealarm++;}
-            if (((aar6[0]-aar6[1])/aar6[1])>0.25){pressurealarm++;}
-
-            if (pressurealarm>3)
-            {
-                alarm2pressure=1;
-            }
-
-            var strainalarm=0;
-            if (((aaar[0]-aaar[1])/aaar[1])>0.2){strainalarm++;}
-            if (((aaar2[0]-aaar2[1])/aaar2[1])>0.2){strainalarm++;}
-            if (((aaar3[0]-aaar3[1])/aaar3[1])>0.2){strainalarm++;}
-            if (((aaar4[0]-aaar4[1])/aaar4[1])>0.2){strainalarm++;}
-            if (((aaar5[0]-aaar5[1])/aaar5[1])>0.2){strainalarm++;}
-            if (((aaar6[0]-aaar6[1])/aaar6[1])>0.2){strainalarm++;}
-
-            if (strainalarm>3)
-            {
-                alarm2strain=1;
-            }
-
-            if (alarm2water && (alarm2pressure|| alarm2strain))
-            {
-                //二级报警操作
-                document.getElementById('header2').className = "main";
-            }
-
-            //三级报警
-            var alarm3=0;
-            var alarm3flag=0;
-            for (var k=0;k<100;k++)
-            {
-                if (ar[k]>99)
-                {
-                    alarm3flag++;
-                }
-                if (ar4[k]>99) //最上层是传感器1和4
-                {
-                    alarm3flag++;
-                }
-            }
-            if (alarm3flag>100)
-            {
-                alarm3=1;
-            }
-            if (alarm3==1)
-            {
-                //三级报警操作
-                document.getElementById('header3').className = "main";
-            }
-
-	var press1=0; 
-	for (var k=1;k<50;k++)
-	{
-		if (ar[k]!=ar[k-1])
-		{
-			press1=1;
-		}
-	}
-	if (press1==0)
-	{
-		document.getElementById('header3').className = "main";	
-	}
-
-
-	var press2=0; 
-	for (var k=1;k<50;k++)
-	{
-		if (ar2[k]!=ar2[k-1])
-		{
-			press2=1;
-		}
-	}
-	
-	if (press2==0)
-	{
-		document.getElementById('header3').className = "main";	
-	}
-
-	var press3=0; 
-	for (var k=1;k<50;k++)
-	{
-		if (ar3[k]!=ar3[k-1])
-		{
-			press3=1;
-		}
-	}
-	
-	if (press3==0)
-	{
-		document.getElementById('header3').className = "main";	
-	}
-
-	var press4=0; 
-	for (var k=1;k<50;k++)
-	{
-		if (ar4[k]!=ar4[k-1])
-		{
-			press4=1;
-		}
-	}
-	
-	if (press4==0)
-	{
-		document.getElementById('header3').className = "main";	
-	}
-
-	var press5=0; 
-	for (var k=1;k<50;k++)
-	{
-		if (ar5[k]!=ar5[k-1])
-		{
-			press5=1;
-		}
-	}
-	
-	if (press5==0)
-	{
-		document.getElementById('header3').className = "main";	
-	}
-
-	var press6=0; 
-	for (var k=1;k<50;k++)
-	{
-		if (ar6[k]!=ar6[k-1])
-		{
-			press6=1;
-		}
-	}	
-	if (press6==0)
-	{
-		document.getElementById('header3').className = "main";	
-	}
+        }
+        if (alarm3flag > 100) {
+            alarm3 = 1;
+        }
+        if (alarm3 == 1) {
+            //三级报警操作
+            document.getElementById('header3').className = "main";
+        }
 
 
 
