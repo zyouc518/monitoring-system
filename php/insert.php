89845331735cd77cb0acbbcbcc1404651afe1b50
Wang Shuhan <harrywsh@126.com>
2018-09-11 18:45:40 +0800

    avoid repeated insertion

--- php/insert.php
+++ php/insert.php
@@ -7,12 +7,39 @@ $position = $_POST['position'];
 $value = $_POST['value'];
 $level = $_POST['level'];
 
-$q = "INSERT INTO alarm (type,name,position,value,level) VALUES ('$type','$name','$position','$value','$level')";
+$g = "SELECT * FROM `parameter` ORDER BY id DESC LIMIT 1";
+$getlevel = $connection->query($g);
 
-$query = mysqli_query($connection, $q);
-
-if ($query) {
-    echo json_encode("Data inserted successfully!");
+$car = mysqli_fetch_assoc($getlevel);
+echo json_encode($car);
+if ($level == '一级') {
+    if ($car['level1'] == 0) {
+        $q = "INSERT INTO alarm (type,name,position,value,level) VALUES ('$type','$name','$position','$value','$level')";
+        $c = "UPDATE parameter SET level1 = 1 WHERE id = 1";
+        $setvalue = mysqli_query($connection, $c);
+        $query = mysqli_query($connection, $q);
+    }
+} else if ($level == '二级') {
+    if ($car['level2'] == 0) {
+        $q = "INSERT INTO alarm (type,name,position,value,level) VALUES ('$type','$name','$position','$value','$level')";
+        $c = "UPDATE parameter SET level2 = 1 WHERE id = 1";
+        $setvalue = mysqli_query($connection, $c);
+        $query = mysqli_query($connection, $q);
+    }
+} else if ($level == '三级') {
+    if ($car['level3'] == 0) {
+        $q = "INSERT INTO alarm (type,name,position,value,level) VALUES ('$type','$name','$position','$value','$level')";
+        $c = "UPDATE parameter SET level3 = 1 WHERE id = 1";
+        $setvalue = mysqli_query($connection, $c);
+        $query = mysqli_query($connection, $q);
+    }
 } else {
-    echo json_encode("Fail to insert data!");
+    $q = "INSERT INTO alarm (type,name,position,value,level) VALUES ('$type','$name','$position','$value','$level')";
+    $query = mysqli_query($connection, $q);
 }
+
+// if ($query) {
+//     echo json_encode("Data inserted successfully!");
+// } else {
+//     echo json_encode("Fail to insert data!");
+// }
--- php/parameterins.php
+++ php/parameterins.php
@@ -4,11 +4,10 @@ $tml = $_GET['tml'];
 $link = mysqli_connect('localhost', 'root', '123456', 'mineral liquefying');
 $q = "TRUNCATE parameter";
 $query = mysqli_query($link, $q);
-$q = "INSERT INTO parameter (density, tml) VALUES ('$density', '$tml')";
+$q = "INSERT INTO parameter (density, tml, level1, level2, level3) VALUES ('$density', '$tml', 0, 0, 0)";
 $query = mysqli_query($link, $q);
 if ($query) {
     echo json_encode("maheng");
 } else {
     echo json_encode("Fail to insert data!");
 }
-?>
