c5be4912fe7bda746dae10665eb3d8cb81721800
Youchen Zhao <33065689+zyouc518@users.noreply.github.com>
2018-10-29 00:18:35 -0400

    change password

--- php/g1.php
+++ php/g1.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "123456", "mineral liquefying");//改成对应的服务器地址、用户名、密码、数据库名称
+$link = mysqli_connect("localhost", "root", "zcq20181123sssri", "mineral liquefying");//改成对应的服务器地址、用户名、密码、数据库名称
 
 $query = "SELECT * FROM `sensor1` ORDER BY Time_1 DESC LIMIT 1200";//改成对应的数据表名称、列名称
 //SELECT * FROM `sensor1` ORDER BY id DESC LIMIT 10; 搜索最后10个
