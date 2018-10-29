efb50abe3ff8088f9bb17d8abd41ffb1897acf71
Youchen Zhao <33065689+zyouc518@users.noreply.github.com>
2018-10-29 00:18:07 -0400

    change password

--- php/parameterload.php
+++ php/parameterload.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost", "root", "123456", "mineral liquefying");//改成对应的服务器地址、用户名、密码、数据库名称
+$link = mysqli_connect("localhost", "root", "zcq20181123sssri", "mineral liquefying");//改成对应的服务器地址、用户名、密码、数据库名称
 
 $query = "SELECT * FROM `parameter` ORDER BY id DESC LIMIT 1";//改成对应的数据表名称、列名称
 
